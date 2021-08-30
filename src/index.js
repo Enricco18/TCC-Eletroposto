46a91ed72137970ea7a74e07d9344a03c1614dc1
Enricco Diniz <enricco.amaral@zup.com.br>
2021-08-30 16:10:48 -0300

    Adicionado WebSocket!

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
 import express from 'express';
-import router from './routes/router';
+// import router from './routes/router';
+import * as WebSocket from 'ws';
+import http from 'http';
 
-const server = express();
+const app = express();
 
-server.use(express.json());
+const server = http.createServer(app);
 
-server.use(router);
+const wss = new WebSocket.Server({server})
+
+wss.on('connection', (ws) => {
+
+    console.log("conectado")
+    //connection is up, let's add a simple simple event
+    ws.on('message', (message) => {
+
+        //log the received message and send it back to the client
+        console.log('received: %s', message);
+        ws.send(`Hello, you sent -> ${message}`);
+    });
+
+    //send immediatly a feedback to the incoming connection    
+    ws.send('Hi there, I am a WebSocket server');
+});
+
+
 
 server.listen((3000), () => {
     console.log("Server listening on PORT: 3000")
